536e3b0d2dd1cc387a726bec1ce9aae93dfe5728
Vedad Yornic <vedadsa@windowslive.com>
2015-02-20 09:47:59 +0100

    Added new classes for rating of restaurant

---
+++ src/ba/tanjir/restaurant/review/Rate.java
@@ -0,0 +1,19 @@
+package ba.tanjir.restaurant.review;
+
+import ba.tanjir.user.Customer;
+
+/**
+ * Class that creates single vote/rate.
+ * It has user ( we'll change Customer into type of PK of customer class), 
+ * customers comment and vote.
+ * TODO votes must be 1-5.
+ * @author vedadzornic
+ *
+ */
+public class Rate {
+	
+	public Customer customerThatVoted;
+	public String comment;
+	public int vote;
+	
+}
---
+++ src/ba/tanjir/restaurant/review/RestaurantReview.java
@@ -0,0 +1,14 @@
+package ba.tanjir.restaurant.review;
+
+import java.util.ArrayList;
+
+/**
+ * Class that has all rates of restaurant.
+ * @author vedadzornic
+ *
+ */
+public class RestaurantReview {
+
+	public String restaurantPDVId;
+	public ArrayList<Rate> allRates; 
+}
